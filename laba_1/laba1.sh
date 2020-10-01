2ae4341938848ffa3bd5f242b4400f094c84ad42
Mikhail <golananime@gmail.com>
2020-10-02 00:15:46 +0300

    s

--- laba_1/laba1.sh
+++ laba_1/laba1.sh
@@ -115,3 +115,24 @@ ls -R | grep "^s" | head  -n 3 | sort -n
 (ls -lRur | head -n 6 | tail -4) 2> /dev/null
 #---------------------------------------------------------
 
+#5 задание------------------------------------------------
+cd /home/s311707/lab0
+
+chmod 777 huntail1
+chmod u+w ledian7
+chmod 777 ledian7/shedinja
+chmod -R 777 nincada0
+#Изменил права доступа для удаления
+
+rm huntail1
+rm ledian7/shedinja
+rm Copy_13
+rm nincada0/altariahuntail
+rm -r nincada0
+rmdir ledian7/empoleon
+#Удалил нужные файлы
+
+chmod u-w ledian7
+#Вернул папкам их права доступа
+#---------------------------------------------------------
+
